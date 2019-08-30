e4fade44c9ba656ce781c20f83c174f884a49071
rushil <rushil@100.64.13.19>
2019-08-30 12:43:01 -0400

    remove java version print statement

--- src/a1/A1Example.java
+++ src/a1/A1Example.java
@@ -9,7 +9,7 @@ public class A1Example {
 		// you know what Eclipse thinks it is using and
 		// so that you can confirm that it is 10.0 or higher.
 		
-		System.out.println("Your Java version: " + System.getProperty("java.version"));
+		// System.out.println("Your Java version: " + System.getProperty("java.version"));
 		
 		// Set up scanner object to read space-separated values
 		// from console input
@@ -19,7 +19,6 @@ public class A1Example {
 		// Read in count of numbers to process
 		
 		int count = scan.nextInt();
-		
 		
 		// Create an array to store numbers.
 		
