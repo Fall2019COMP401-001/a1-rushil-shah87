9d14e13f87aed8dd084cf3d0b755eb352b5704b4
rushil <rushil@100.64.13.19>
2019-08-30 13:12:07 -0400

    completed novice task

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,32 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-
 		// Your code follows here.
 		
+		Scanner scan = new Scanner(System.in);
+		int num_customers = scan.nextInt();
+		
+		String fname = "";
+		String lname = "";
+		int num_items = 0;
+		int quant = 0;
+		double price = 0.00;
+		double total_cost = 0.00;
+		String item = "";
+		
+		for (int i=0; i<num_customers; i++) {
+			total_cost = 0.00;
+			fname = scan.next();
+			lname = scan.next();
+			num_items = scan.nextInt();
+			for (int j=0; j<num_items; j++) {
+				quant = scan.nextInt();
+				item = scan.next();
+				price = scan.nextDouble();
+				total_cost += (quant*price);
+			}
+		System.out.println(fname.charAt(0)+". "+lname+": "+String.format("%.2f",total_cost));
+		}
+				
 	}
 }
